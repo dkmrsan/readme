aaab03b156b47d247ec7647e4f6eb137247a142d
大康 <@DESKTOP-OVRVTVK>
2019-05-06 09:24:52 +0800

    ojbk

---
+++ asd/src/asd/a.java
@@ -0,0 +1,12 @@
+package asd;
+
+public class a {
+	public static void main(String[] args) {
+		Student s1 = new Student(1,"A","ÄÐ",19);
+		
+	      Student s2 = new Student(1,"A","ÄÐ",19);
+
+	      System.out.println(s1.equals(s2));
+	}
+
+}
---
+++ asd/src/asd/b.java
@@ -0,0 +1,12 @@
+package asd;
+
+public class b {
+	static int i;
+	 public void a(){
+		i++;
+	}
+	
+	int d;
+	private int e;
+
+}
